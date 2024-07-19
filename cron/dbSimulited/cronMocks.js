637498dde10c3b892781deb34db3ee5ddaafc51d
Alexander Gorbunov <agazipov@gmail.com>
2024-07-19 12:16:27 +0500

    date fix

--- cron/dbSimulited/cronMocks.js
+++ cron/dbSimulited/cronMocks.js
@@ -7,7 +7,7 @@ const path = require('path');
 // updates moks files every midnight
 module.exports.job = CronJob.from({
 	// cronTime: '00 * * * * *',
-	cronTime: '00 00 00 * * *',
+	cronTime: '00 05 00 * * *',
 	onTick: function () {
 		console.log('mock cron worked');
 		const events = [];
